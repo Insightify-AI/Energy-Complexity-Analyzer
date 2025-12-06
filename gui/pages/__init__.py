6fa9952ec304d541dda266ba4e29b140314cbd03
fatihkorkmaz7 <korkmazfatih950@gmail.com>
2025-12-06 22:14:17 +0300

    eski dosyalar arşive kaldırıldı

--- gui/pages/__init__.py
+++ gui/pages/__init__.py
@@ -3,13 +3,11 @@ Sayfa Modülleri
 ===============
 """
 
-from gui.pages.test_runner import TestRunnerPage
 from gui.pages.comparison import ComparisonPage
 from gui.pages.history import HistoryPage
 from gui.pages.real_energy import RealEnergyPage
 
 __all__ = [
-    'TestRunnerPage', 
     'ComparisonPage',
     'HistoryPage',
     'RealEnergyPage'
